0e9e53058d9c9d67596766e377ecc783a3610740
Thien Chi <ltvthienchi@gmail.com>
2018-10-15 01:43:40 +0700

    Add project files.

---
+++ AptitudeTestOnline/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AptitudeTestOnline.MvcApplication" Language="C#" %>
---
+++ AptitudeTestOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AptitudeTestOnline.Startup))]
+namespace AptitudeTestOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
