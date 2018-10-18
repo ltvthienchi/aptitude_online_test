680b4c9a613440ab789bad01e42f611c15a30aa8
Thien Chi <ltvthienchi@gmail.com>
2018-10-19 02:27:58 +0700

    update admin and caditite site

---
+++ AptitudeTestOnline/Areas/Manager/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AptitudeTestOnline.Areas.Manager.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class HomeController : Controller
+    {
+        // GET: Manager/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AptitudeTestOnline/Areas/Manager/ManagerAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace AptitudeTestOnline.Areas.Manager
+{
+    public class ManagerAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Manager";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Manager_default",
+                "Manager/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "AptitudeTestOnline.Areas.Manager.Controllers" } 
+            );
+        }
+    }
+}
