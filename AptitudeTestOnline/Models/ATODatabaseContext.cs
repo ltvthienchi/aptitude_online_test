0a613c19699377232e7b483d9cd112f7f4ccbc80
Thien Chi <ltvthienchi@gmail.com>
2018-10-22 19:30:19 +0700

    update

---
+++ AptitudeTestOnline/Models/ATODatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AptitudeTestOnline.Models
+{
+    public class ATODatabaseContext: DbContext
+    {
+        public ATODatabaseContext() : base("name=DefaultConnection") { }
+        public DbSet<TypeOfQuestionModel> TypeOfQuestionModel { get; set; }
+    }
+}
---
+++ AptitudeTestOnline/Models/TypeOfQuestionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AptitudeTestOnline.Models
+{
+    public class TypeOfQuestionModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int TypeOfQuestion { get; set; }
+        public string NameTypeOfQuestion { get; set; }
+
+        public ICollection<TypeOfQuestionModel> TypeOfQuestionModels { get; set; }
+    }
+}
