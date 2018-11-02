e819f9b2becec4efaf9794741793f8cc7c71b213
nguyenhuudam <nguyenhuudam0801@gmail.com>
2018-11-02 18:04:13 +0700

    update

--- AptitudeTestOnline/Areas/Manager/Controllers/UsersController.cs
+++ AptitudeTestOnline/Areas/Manager/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using AptitudeTestOnline.Models;
+using PagedList;
 
 namespace AptitudeTestOnline.Areas.Manager.Controllers
 {
@@ -14,12 +15,33 @@ namespace AptitudeTestOnline.Areas.Manager.Controllers
     {
         private ATODatabaseContext db = new ATODatabaseContext();
         // GET: Manager/Users
-        public ActionResult Index()
+        public ActionResult Index(int? page, string searchString, string currentFilter)
         {
-            
+            var ListResult = from q in db.AccountModels select q;
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                ListResult = db.AccountModels.Where(s => s.Name.Contains(searchString));
+            }
+
+
+            ListResult = ListResult.OrderByDescending(q => q.AccountID);
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
             ViewData["ListAccount"] = db.AccountModels.ToList();
             ViewData["ListRegister"] = db.DetailsRegistrations.OrderByDescending(a => a.Mark).ToList();
-            return View();
+            return View(ListResult.ToPagedList(pageNumber, pageSize));
             
         }
     }
